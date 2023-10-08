729af287bb4e3c337f7391722219a787f5cc795b
Pavel Mezencev <mezencev24@mail.ru>
2023-10-08 19:57:24 +0300

    add not found page and page loader widjet

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { About } from 'pages/about';
 import { Home } from 'pages/home';
+import { NotFound } from 'pages/notFound';
 import { RouteProps } from 'react-router-dom';
 
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
+  NOT_FOUND = 'notFound',
 }
 
 export const routePaths: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.ABOUT]: {
     path: routePaths.about,
     element: <About />,
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: routePaths.notFound,
+    element: <NotFound />,
   },
 };
