2f81d73a2a2342b8dc4d782770e8e5799a84643c
Pavel Mezencev <mezencev24@mail.ru>
2023-10-11 23:16:12 +0300

    update sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,7 +1,12 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/ThemeSwitcher';
+import { t } from 'i18next';
 import { useEffect, useState } from 'react';
-import { Button } from 'shared/ui/Button/Button';
+import AboutIcon from 'shared/assets/icons/about.svg';
+import HomeIcon from 'shared/assets/icons/home.svg';
+import { routePaths } from 'shared/config/routeConfig/routeConfig';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import cls from './Sidebar.module.scss';
 
@@ -35,12 +40,37 @@ export const Sidebar = ({ className }: SidebarProps) => {
         className,
       ])}
     >
-      <Button data-testid='sidebar-toggle' onClick={toggleCollapsed}>
-        toggle
+      <div className={cls.links}>
+        <AppLink
+          to={routePaths.main}
+          theme={AppLinkTheme.INVERTED}
+          className={cls.item}
+        >
+          <HomeIcon className={cls.icon} />
+          <span className={cls.link}>{t('Главная')}</span>
+        </AppLink>
+        <AppLink
+          to={routePaths.about}
+          theme={AppLinkTheme.INVERTED}
+          className={cls.item}
+        >
+          <AboutIcon className={cls.icon} />
+          <span className={cls.link}>{t('О нас')}</span>
+        </AppLink>
+      </div>
+      <Button
+        data-testid='sidebar-toggle'
+        onClick={toggleCollapsed}
+        className={cls.toggleBtn}
+        square
+        size={ButtonSize.SIZE_M}
+        theme={ButtonTheme.PRIMARY}
+      >
+        {collapsed ? '>' : '<'}
       </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher />
+        <LangSwitcher short={collapsed} />
       </div>
     </aside>
   );
